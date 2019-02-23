d238c543ea368960802fd52bdf1ea0a123e3b3c9
Ivan Gonzalez <ivandario1998@gmail.com>
2019-02-23 14:55:09 -0500

    Generate token for sign up user

--- controllers/users.js
+++ controllers/users.js
@@ -1,12 +1,23 @@
+const JWT = require('jsonwebtoken');
 const User = require('../models/user');
+const { JWT_SECRET } = require('../configuration');
+
+signToken = (user) => {
+    return JWT.sign({
+        iss: 'find-room',
+        sub: user.id,
+        iat: new Date().getTime(),
+        exp: new Date().setDate(new Date().getDate() + 1)
+    }, JWT_SECRET);
+}
 
 module.exports = {
     signUp: async (req, res, next) => {
-        const { email, password } = req.value.body;          
+        const { email, password } = req.value.body;
 
         const foundUser = await User.findOne({ email });
-        if (foundUser) { 
-            return res.status(403).send({ error: 'Email is already in used'})
+        if (foundUser) {
+            return res.status(403).send({ error: 'Email is already in used' })
         };
 
         const newUser = new User({
@@ -14,7 +25,10 @@ module.exports = {
             password
         });
         await newUser.save();
-        res.json({ user: 'Created'})
+
+        const token = signToken(newUser);
+
+        res.status(200).json({ token });
     },
 
     signIn: async (req, res, next) => {
