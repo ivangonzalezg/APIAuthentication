26c40a7ae0376f05dde26e4c35b925468ec7a52f
Ivan Gonzalez <ivandario1998@gmail.com>
2019-02-23 21:40:06 -0500

    Password encrypted with bcryptjs

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
@@ -11,6 +12,17 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true
+    }
+});
+
+userSchema.pre('save', async function(next) {
+    try {
+        const salt = await bcrypt.genSalt(10);
+        const passwordHash = await bcrypt.hash(this.password, salt);
+        this.password = passwordHash;
+        next();
+    } catch (error) {
+        next(error);
     }
 });
 
